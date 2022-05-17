1c09f5bc2d72741bec6ebe163bacd100acbd3003
Nico <nico.riedlinger@gmx.de>
2022-05-17 20:21:32 +0200

    Center in flex box

--- app/components/Styles.js
+++ app/components/Styles.js
@@ -10,6 +10,8 @@ module.exports = StyleSheet.create({
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
+        height: '100%',
+        width: '100%',
     },
     fullWidth: {
         width: '100%',
