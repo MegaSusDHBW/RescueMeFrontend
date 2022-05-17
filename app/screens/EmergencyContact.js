54a40d906bcad7d16272b4480ba441d35cd564a8
LukasSieber <i19046@hb.dhbw-stuttgart.de>
2022-05-17 22:50:46 +0200

    first try with alerts with @SImonMeltzer-Git, @Ic3Fighter

--- app/screens/EmergencyContact.js
+++ app/screens/EmergencyContact.js
@@ -1,58 +1,61 @@
-import React,{useState,useEffect} from 'react';
-import {Alert ,View,Text, TextInput,Button } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {Alert} from 'react-native';
 import * as SecureStore from 'expo-secure-store';
-
-
+import style from "../components/Styles";
+import {Button, Input, Text, View} from "native-base";
+import DatePicker from "react-native-date-picker";
 
 
 function EmergencyContact({navigation}) {
-       
-    useEffect( async () => {
+
+    useEffect(async () => {
         await getUserMail()
-     if(birthDate === null){   
-      const response = await fetch(
-          'http://10.0.2.2:5000/get-emergencycontact?email=' + userMail,
-         );
-        const data = await response.json();
-        let birthDate =  data.emergencyBirthday
-        setBirthDate(birthDate)
-        let email =  data.emergencyEmail
-        setEmail(email)
-        let firstName = data.emergencyFirstname
-        setFirstname(firstName)
-        let lastName = data.emergencyLastname
-        setLastname(lastName)
-        let phone = data.emergencyPhone
-        setPhoneNumber(phone)         
-      }    
+        if (birthDate === null) {
+            const response = await fetch(
+                'http://10.0.2.2:5000/get-emergencycontact?email=' + userMail,
+            );
+            const data = await response.json();
+            let birthDate = data.emergencyBirthday
+            setBirthDate(birthDate)
+            let email = data.emergencyEmail
+            setEmail(email)
+            let firstName = data.emergencyFirstname
+            setFirstname(firstName)
+            let lastName = data.emergencyLastname
+            setLastname(lastName)
+            let phone = data.emergencyPhone
+            setPhoneNumber(phone)
+        }
     });
-  
-    async function getUserMail(){
+
+    async function getUserMail() {
         let store = await SecureStore.getItemAsync('email');
-        
-          return setUsermail(store) 
-      }    
-    const contact={
-        firstName:'',
-        lastName:'',
-        birthDate:'',
-        phoneNumber:'',
-        email:'',
+
+        return setUsermail(store)
+    }
+
+    const contact = {
+        firstName: '',
+        lastName: '',
+        birthDate: '',
+        phoneNumber: '',
+        email: '',
         userMail: ''
     }
-    const [firstName,setFirstname] = useState(null);
+    const [firstName, setFirstname] = useState(null);
     const [lastName, setLastname] = useState(null);
-    const [birthDate,setBirthDate] =useState(null);
-    const [phoneNumber, setPhoneNumber] =useState(null);
-    const [email,setEmail] = useState(null);
-    const [userMail,setUsermail] =useState(null);
+    const [birthDate, setBirthDate] = useState(null);
+    const [phoneNumber, setPhoneNumber] = useState(null);
+    const [email, setEmail] = useState(null);
+    const [userMail, setUsermail] = useState(null);
     contact.firstName = firstName;
     contact.lastName = lastName;
     contact.birthDate = birthDate;
     contact.phoneNumber = phoneNumber;
     contact.email = email;
     contact.userMail = userMail;
-    function handleNavigationHome(){
+
+    function handleNavigationHome() {
         navigation.navigate('Home')
     }
 
@@ -60,41 +63,77 @@ function EmergencyContact({navigation}) {
         console.log(JSON.stringify(contact));
 
         try {
-            const requestOptions = 
-            {
-            
-                method: 'POST',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify(contact)
-            };
-                console.log("POST")
-                console.log(JSON.stringify(contact))
+            const requestOptions =
+                {
+                    method: 'POST',
+                    headers: {'Content-Type': 'application/json'},
+                    body: JSON.stringify(contact)
+                };
+            console.log("POST")
+            console.log(JSON.stringify(contact))
 
-                await fetch(
-                   'http://10.0.2.2:5000/set-emergencycontact',
-                    requestOptions,
-                  ).then(response => { if(response.ok){
+            await fetch(
+                'http://10.0.2.2:5000/set-emergencycontact',
+                requestOptions,
+            ).then(response => {
+                if (response.ok) {
                     handleNavigationHome();
-                  }else{
+                } else {
                     Alert.alert('Upps')
-                  };
-                  });
-                } catch (error) {
-                  console.error(error);
                 }
-              };
+                ;
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
-     
-    
     return (
-     <View>
-         <TextInput placeholder='Vorname' onChangeText={(value) => setFirstname(value)} value={firstName} />
-         <TextInput placeholder='Nachname' onChangeText={(value) => setLastname(value)} value={lastName} />
-         <TextInput placeholder='Geburtsdatum' onChangeText={(value) => setBirthDate(value)} value={birthDate} />
-         <TextInput placeholder='Telefonnummer' onChangeText={(value) => setPhoneNumber(value)} value={phoneNumber} />
-         <TextInput placeholder='Email' onChangeText={(value) => setEmail(value)} value={email} />
-         <Button title='Bestätigen'  onPress={handleSubmit}/>
-     </View>
+        <View style={[style.wrapper, style.flex]}>
+            <View style={[style.fullWidth, style.marginForm]}>
+                <Text>Vorname</Text>
+                <Input style={style.fullWidth}
+                       variant="custom"
+                       onChangeText={(value) => setFirstname(value)}
+                       value={firstName}
+                       />
+            </View>
+            <View style={[style.fullWidth, style.marginForm]}>
+                <Text>Nachname</Text>
+                <Input style={style.fullWidth}
+                       variant="custom"
+                       onChangeText={(value) => setLastname(value)}
+                       value={lastName}
+                       />
+            </View>
+            <View style={[style.fullWidth, style.marginForm]}>
+                <Text>Geburtsdatum</Text>
+                <Input style={style.fullWidth}
+                       variant="custom"
+                       onChangeText={(value) => setBirthDate(value)}
+                       value={birthDate}
+                       />
+            </View>
+            <View style={[style.fullWidth, style.marginForm]}>
+                <Text>Telefonnummer</Text>
+                <Input style={style.fullWidth}
+                       variant="custom"
+                       onChangeText={(value) => setPhoneNumber(value)}
+                       value={phoneNumber}
+                       />
+            </View>
+            <View style={[style.fullWidth, style.marginForm]}>
+                <Text>E-Mail</Text>
+                <Input style={style.fullWidth}
+                       variant="custom"
+                       onChangeText={(value) => setEmail(value)}
+                       value={email}
+                       />
+            </View>
+            <View style={style.fullWidth}>
+                <Button style={style.marginForm} onPress={handleSubmit}><Text>Bestätigen</Text></Button>
+            </View>
+        </View>
     );
 }
 
--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,79 +1,118 @@
-import React, { useEffect, useState } from 'react';
-import { Alert } from 'react-native';
-import { Input, Button, View, Text, Image, Icon } from 'native-base';
+import React, {useEffect, useState} from 'react';
+import {Alert, Button, Center, CloseIcon, HStack, IconButton, Image, Text, View, VStack} from 'native-base';
 import * as Location from '../helper/LocationHelper';
 import * as SecureStore from 'expo-secure-store'
+import style from "../components/Styles";
 
-function HomeScreen({ navigation }) {
-  const style = require('../components/Styles.js');
-  const [what3Words, set3Words] = useState(null);
-  const [email, setEmail] = useState('test');
-  const [loc, setLocation] = useState(null);
-  let location = Location.getLocation();
+function HomeScreen({navigation}) {
+    const style = require('../components/Styles.js');
+    const [what3Words, set3Words] = useState(null);
+    const [email, setEmail] = useState('test');
+    const [loc, setLocation] = useState(null);
+    let location = Location.getLocation();
+    const [errorMessage, setErrorMessage] = useState(null);
 
 
-  useEffect(async () => {
-    try {
-      let storeEmail = await SecureStore.getItemAsync('email');
-      console.log('store ' + storeEmail);
-      console.log(email)
-      setEmail(storeEmail);
-      const requestOptions =
-      {
+    useEffect(async () => {
+        try {
+            let storeEmail = await SecureStore.getItemAsync('email');
+            console.log('store ' + storeEmail);
+            console.log(email)
+            setEmail(storeEmail);
+            const requestOptions =
+                {
 
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(loc)
-      };
-      console.log("POST")
-      console.log(loc)
+                    method: 'POST',
+                    headers: {'Content-Type': 'application/json'},
+                    body: JSON.stringify(loc)
+                };
+            console.log("POST")
+            console.log(loc)
 
-      await fetch(
-        'http://10.0.2.2:5000/get-geodata',
-        requestOptions,
-      ).then(response => {
-        if (response.ok) {
-          console.log(JSON.parse(response.body, 'words'));
-          set3Words(JSON.parse(response.body, 'words'));
+            await fetch(
+                'http://10.0.2.2:5000/get-geodata',
+                requestOptions,
+            ).then(response => {
+                if (response.ok) {
+                    console.log("RESPONSE OKAY")
+                    const data = response.json()
 
-        } else {
-          Alert.alert('Ups')
+                    let what3words = data.words;
+                    set3Words(what3words);
+                } else {
+                    console.log("RESPONSE NOT OKAY")
+                    const data = response.json()
+                    let errorMessage = {
+                        status: "error",
+                        title: data.words
+                    };
+                    setErrorMessage(errorMessage);
+
+                }
+            });
+        } catch (error) {
+            console.error(error);
         }
-        ;
-      });
-    } catch (error) {
-      console.error(error);
-    }
-  });
+    });
 
-  function handleNavQR() {
-    navigation.navigate('QRCodeScanner')
-  };
-  return (
-    <View style={[style.wrapper, style.flex, style.flexStart, style.paddingTop]}>
-      <View style={style.marginForm}>
-        <Text style={style.textCenter}>Willkommen,</Text>
-        <Text style={style.textCenter}>{email}!</Text>
-      </View>
-      {/* <Text style={style.fullWidth}>{email}</Text> */}
-      {/* <View> */}
-      <Image key={new Date().getTime()} source={{
-        uri: 'http://10.0.2.2:5000/create-qrcode?email=' + email + '&date=' + new Date,
-        cache: 'reload'
-      }} style={[{ width: 'auto', height: '55%' }, style.fullWidth, style.marginForm]} />
-      <Button
-        onPress={handleNavQR}
-        style={[style.fullWidth, style.marginForm]}>
-        <Text>Gesundheitsdaten hinzufügen</Text>
-      </Button>
-      {/* </View> */}
-      <View style={style.fullWidth}>
-        <Text style={style.textCenter}>GPS-Position</Text>
-        <Text>what3words:</Text>
-        <Text>///{what3Words}</Text>
-      </View>
-    </View>
-  );
+    function showErrorMessage() {
+        return (
+            <Center>
+                <Alert w="100%" h="55%" status={errorMessage.status}>
+                    <VStack space={2} flexShrink={1} w="100%">
+                        <HStack flexShrink={1} space={2} justifyContent="space-between">
+                            <HStack space={2} flexShrink={1}>
+                                <Alert.Icon mt="1"/>
+                                <Text fontSize="md" color="coolGray.800">
+                                    {errorMessage.title}
+                                </Text>
+                            </HStack>
+                            <IconButton variant="unstyled" _focus={{
+                                borderWidth: 0
+                            }} icon={<CloseIcon size="3" color="coolGray.600"/>}/>
+                        </HStack>
+                    </VStack>
+                </Alert>
+            </Center>
+        )
+    }
+
+    function handleNavQR() {
+        navigation.navigate('QRCodeScanner')
+    };
+
+    if (errorMessage != null) {
+
+        return showErrorMessage();
+    } else {
+        return (
+
+            <View style={[style.wrapper, style.flex, style.flexStart, style.paddingTop]}>
+                <View style={style.marginForm}>
+                    <Text style={style.textCenter}>Willkommen,</Text>
+                    <Text style={style.textCenter}>{email}!</Text>
+                </View>
+                <Image key={new Date().getTime()} source={{
+                    uri: 'http://10.0.2.2:5000/create-qrcode?email=' + email + '&date=' + new Date,
+                    cache: 'reload'
+                }} style={[{width: 'auto', height: '55%'}, style.fullWidth, style.marginForm]}/>
+                <Button
+                    onPress={handleNavQR}
+                    style={[style.fullWidth, style.marginForm]}>
+                    <Text>Gesundheitsdaten hinzufügen</Text>
+                </Button>
+                {/* </View> */}
+                <View style={style.fullWidth}>
+                    <Text style={style.textCenter}>GPS-Position</Text>
+                    <Text>what3words:</Text>
+                    <Text>///{what3Words}</Text>
+                </View>
+            </View>
+
+        )
+    }
+
+
 }
 
 export default HomeScreen;
