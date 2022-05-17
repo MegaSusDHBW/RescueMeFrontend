54799ac80a5c7c43671f47984ac4795cad05df66
Nico <nico.riedlinger@gmx.de>
2022-05-17 19:12:04 +0200

    Made Forgot PW nice

--- app/screens/ForgotPasswordScreen.js
+++ app/screens/ForgotPasswordScreen.js
@@ -1,65 +1,92 @@
-import React, {useState} from 'react';
-import { StyleSheet,TextInput,View, SafeAreaView,Button,Text} from 'react-native';
+import React, { useState } from 'react';
+import { Alert, SafeAreaView } from 'react-native';
+import { Input, Button, View, Text, Image } from 'native-base';
 
+function ForgotPasswordScreen({ navigation }) {
+  const style = require('../components/Styles');
+  const [password, setPassword] = useState(null);
+  const [passwordConfirm, setPasswordConfirm] = useState(null)
+  const [email, setEmail] = useState(null)
+  const newPassword = {
+    email: '',
+    password: '',
+    passwordConfirm: '',
+  };
+  newPassword.password = password
+  newPassword.passwordConfirm = passwordConfirm
+  newPassword.email = email
+  //TODO add URL
+  const handleSubmit = async () => {
+    try {
+      const requestOptions =
+      {
 
-function ForgotPasswordScreen(props) {
-    const [password,setPassword] = useState(null);
-    const [passwordConfirm, setPasswordConfirm] = useState(null)
-    const [email,setEmail]= useState(null)
-    const newPassword = {
-        email:'',
-        password:'',
-        passwordConfirm:'',
-    };
-    newPassword.password = password
-    newPassword.passwordConfirm = passwordConfirm
-    newPassword.email = email
-    //TODO add URL
-    const handleSubmit= async () => {
-        try {
-    const requestOptions = 
-    {
-    
         method: 'POST',
-        headers: {'Content-Type': 'application/json'},
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(newPassword)
-    };
-        console.log("POST")
-        console.log(JSON.stringify(newPassword))
-        console.log(requestOptions.body);
-        await fetch(
-           'http://10.0.2.2:5000/forget-password',
-            requestOptions,
-          ).then(response => {
-            response.json().then(data => {
-            Alert.alert('Post created at : ');
-            });
-          });
-        } catch (error) {
-          console.error(error);
-        }
       };
+      console.log("POST")
+      console.log(JSON.stringify(newPassword))
+      console.log(requestOptions.body);
+      await fetch(
+        'http://10.0.2.2:5000/forget-password',
+        requestOptions,
+      ).then(response => {
+        response.json().then(data => {
+          Alert.alert('Post created at : ');
+        });
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    return (
-        <SafeAreaView style={styles.container}>    
-        <View>
-        <TextInput placeholder='Email' onChangeText={(value) => setEmail(value)} value={email} />
-        <TextInput placeholder='Passwort' onChangeText={(value) => setPassword(value )} value={password} secureTextEntry={true}/>
-        <TextInput placeholder='Passwort wiederholen' onChangeText={(value) => setPasswordConfirm(value )} value={passwordConfirm} secureTextEntry={true}/>
-        <Button title='Bestätigen' onPress={handleSubmit}/>
+  function handleNavigationLogin() {
+    navigation.navigate('Login');
+  }
+
+  return (
+    <SafeAreaView>
+      <View style={[style.wrapper, style.flex]}>
+        {/* <Image source={require('../assets/LogoText.png')}
+            alt="Rescue Me Logo" size={'1/2'} /> */}
+        <View style={[style.fullWidth, style.marginForm]}>
+          <Text>E-Mail</Text>
+          <Input style={style.fullWidth}
+            variant="custom"
+            onChangeText={(value) => setEmail(value)}
+            value={email} />
         </View>
-        </SafeAreaView>
-    );
+        <View style={[style.fullWidth, style.marginForm]}>
+          <Text>Passwort</Text>
+          <Input style={style.fullWidth}
+            variant="custom"
+            onChangeText={(value) => setPassword(value)}
+            value={password}
+            secureTextEntry={true} />
+        </View>
+        <View style={[style.fullWidth, style.marginForm]}>
+          <Text>Passwort wiederholen</Text>
+          <Input style={style.fullWidth}
+            variant="custom"
+            onChangeText={(value) => setPasswordConfirm(value)}
+            value={passwordConfirm}
+            secureTextEntry={true} />
+        </View>
+        <View style={style.fullWidth}>
+          <Button onPress={handleSubmit} style={style.marginForm}>
+            <Text>Bestätigen</Text>
+          </Button>
+          <Button
+            onPress={handleNavigationLogin}
+            style={style.marginForm}
+            variant={'unstyled'}>
+            <Text>Abbrechen</Text>
+          </Button>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 }
 
 export default ForgotPasswordScreen;
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        width:"100%",        
-        justifyContent: 'center',
-        padding: 20,
-      },
-    
-})
